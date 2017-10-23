0c02ff1499526dfbfa69862ab32822c365aa6940
Eric Dobbertin <eric@dairystatedesigns.com>
2017-10-23 10:51:48 -0500

    Update S3 bucket

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 echo "Rebuilding..."
 mkdocs build --clean
 echo "Deploying to S3..."
-aws s3 cp $PWD/site s3://composable-forms/ --recursive --acl public-read
+aws s3 cp $PWD/site s3://forms.dairystatedesigns.com/ --recursive --acl public-read
 echo "Done!"
